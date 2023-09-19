13870049be338a8ed92c811ffb0d7917505b867d
DespaiiR <vlad@hozin.ru>
2023-09-19 19:30:03 +0300

    исправления

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,9 @@
 let navMain = document.querySelector(".main-nav");
 let navBurger = document.querySelector(".main-nav__burger");
 
+navMain.classList.remove("main-nav--active");
+navMain.classList.remove("main-nav--no-js");
+
 navBurger.addEventListener('click', function() {
   navMain.classList.toggle("main-nav--active");
 })
